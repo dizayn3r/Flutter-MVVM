305f450e972efcd08708928a0eb4deca324f06e0
Shashi Kant <dizayn3r@gmail.com>
2022-10-23 20:23:06 +0530

    Edit some parameters

--- lib/res/color.dart
+++ lib/res/color.dart
@@ -5,7 +5,6 @@ class AppColors {
 
   static const Color blackColor = Color(0xFF212121);
   static const Color whiteColor = Color(0xFFFFFFFF);
-
   static const Color primary = Color(0xFFEE2F4D);
 
 }
--- lib/view_model/services/splash_services.dart
+++ lib/view_model/services/splash_services.dart
@@ -1,22 +1,16 @@
 
-import 'package:flutter/foundation.dart';
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_mvvm_demo/models/user_model.dart';
 import 'package:flutter_mvvm_demo/utils/routes/routes_name.dart';
 import 'package:flutter_mvvm_demo/view_model/user_view_model.dart';
 
 class SplashServices {
-
-
   Future<UserModel> getUserDate() => UserViewModel().getUser();
-
-
   void checkAuthentication(BuildContext context)async{
-
     getUserDate().then((value)async{
-
-      print(value.token.toString());
-
+      String token = "${value.token}";
+      log("Token: $token");
       if(value.token.toString() == 'null' || value.token.toString() == ''){
         await Future.delayed(const Duration(seconds: 3));
         Navigator.pushReplacementNamed(context, RoutesName.login);
@@ -24,11 +18,8 @@ class SplashServices {
         await  Future.delayed(const Duration(seconds: 3));
         Navigator.pushReplacementNamed(context, RoutesName.home);
       }
-
     }).onError((error, stackTrace){
-      if(kDebugMode){
-        print(error.toString());
-      }
+      log("Error: $error");
     });
 
   }
